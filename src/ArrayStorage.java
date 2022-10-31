92d8bdaf32e3527d6302ae685d2fb58a97e9aa7e
Vasili Bandarenka <darken123@inbox.ru>
2022-10-31 12:12:33 +0300

    прописал комментарии, рефакторинг кода

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,21 +5,21 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
-        int storageSize = this.size();
-        for (int i = 0; i < storageSize; i++) {
+        int storageSize = this.size(); //сохраняю фиксирую число резюме
+        for (int i = 0; i < storageSize; i++) { //стираю резюме
             storage[i] = null;
         }
     }
 
     void save(Resume r) {
-        if (r.uuid!=null) {
-            storage[this.size()] = r;
+        if (r.uuid != null) { //если на входе не null
+            storage[this.size()] = r; //сохраняю в последнюю свободную ячейку
         }
     }
 
     Resume get(String uuid) {
         int i = 0;
-        while (storage[i] != null && !storage[i].uuid.equals(uuid)) {
+        while (storage[i] != null && !storage[i].uuid.equals(uuid)) { //ищем uuid пока ячейки не null
             i++;
         }
         return storage[i];
@@ -27,12 +27,11 @@ public class ArrayStorage {
 
     void delete(String uuid) {
         int i = 0;
-        while (this.get(uuid) != null) { //проверяем, если такой элемент есть в массиве
-            while (storage[i] != null && storage[i].uuid.equals(uuid)) { //сдвигаю по одному
-                int storageSize = this.size();
-                for (int k = i; k < storageSize; k++) {
-                    storage[k] = storage[k + 1];
-                }
+        while (this.get(uuid) != null) { //пока элементы равные uuid есть в резюме, крутим цикл
+            while (storage[i] != null && storage[i].uuid.equals(uuid)) { //ищем резюме uuid
+                int storageSize = this.size(); //сохранил число резюме
+                //сдвигаю массив начиная с элемента i
+                System.arraycopy(storage, i + 1, storage, i, storageSize - i);
             }
             i++;
         }
@@ -42,15 +41,15 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int storageSize = this.size();
-        Resume[] r = new Resume[storageSize];
-        if (this.size() >= 0) System.arraycopy(storage, 0, r, 0, this.size());
+        Resume[] r = new Resume[this.size()]; //создаем новый массив длиной с число резюме
+        //копируем в него часть массива с резюме
+        System.arraycopy(storage, 0, r, 0, this.size());
         return r;
     }
 
     int size() {
         int i = 0;
-        while (storage[i] != null) {
+        while (storage[i] != null) { //пробегаем по резюме пока не null
             i++;
         }
         return i;
