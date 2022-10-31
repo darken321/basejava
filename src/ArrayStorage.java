c70f6ca37a3fdf8486a5e7c09e70396025c442fd
Vasili Bandarenka <darken123@inbox.ru>
2022-10-31 10:32:29 +0300

    рефакторинг метода save исправил delete и clear

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,5 +1,3 @@
-import java.util.Objects;
-
 /**
  * Array based storage for Resumes
  */
@@ -7,47 +5,44 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        int storageSize = this.size();
+        for (int i = 0; i < storageSize; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
-        int i = 0;
-        while (storage[i] != null ) { //ищем пустой элемент
-            i++;
-        }
-        storage[i] = r;
+        storage[this.size()] = r;
     }
 
     Resume get(String uuid) {
         int i = 0;
-        while (storage[i]!=null&&!storage[i].uuid.equals(uuid)) {
+        while (storage[i] != null && !storage[i].uuid.equals(uuid)) {
             i++;
         }
         return storage[i];
     }
 
     void delete(String uuid) {
-        //uuid = "345";
-        int j = 0;
-//        for (int i = 0; i < this.size(); i++) {
-//
-//        }
-        System.out.println("get " + uuid + " = " + this.get(uuid)); //нашел нужную строку
-        while (this.get(uuid)!=null & j<10 ) { //можно заменить на IF если искать один элемент while чтоб удалить все
-          System.out.println("нашел " + ++j);
-          //тут надо делать поиск элемента равного uuid с индексом и удаление его
+        int i = 0;
+        while (this.get(uuid) != null) { //проверяем, если такой элемент есть в массиве
+            while (storage[i] != null && storage[i].uuid.equals(uuid)) { //сдвигаю по одному
+                int storageSize = this.size();
+                for (int k = i; k < storageSize; k++) {
+                    storage[k] = storage[k + 1];
+                }
+            }
+            i++;
         }
-//        while (this.get(uuid)!=null & j<10) {  //тут я тестирую нашел ли строку uuid в storage
-//            System.out.println("нашел " + ++j); //лимит в 10 ибо программа уходит к границе int
-//        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] r= new Resume[this.size()];
+        int storageSize = this.size();
+        Resume[] r = new Resume[storageSize];
         if (this.size() >= 0) System.arraycopy(storage, 0, r, 0, this.size());
-        //return new Resume[0];
         return r;
     }
 
@@ -55,7 +50,7 @@ public class ArrayStorage {
         int i = 0;
         while (storage[i] != null) {
             i++;
-        };
+        }
         return i;
     }
 }
