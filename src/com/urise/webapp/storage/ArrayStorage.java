8449be641a2ab2f3bf7b0380ff90978a313c7c1b
Vasili Bandarenka <darken123@inbox.ru>
2022-11-02 17:55:15 +0300

    добавил класс update
    класс update ищет резюме в storage и если есть, то сохраняет переденное в параметре резюме в storage

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -8,13 +8,25 @@ import com.urise.webapp.model.Resume;
 public class ArrayStorage {
     private final int MAX_STORAGE = 10000;
     private Resume[] storage = new Resume[MAX_STORAGE];
-    private int size; //число резюме
+    private int size; // число резюме
 
     public void clear() {
-        for (int i = 0; i < size; i++) { //стираю резюме
+        for (int i = 0; i < size; i++) { // стираю резюме
             storage[i] = null;
         }
         size = 0;
+    }
+
+    public void update(Resume r) {
+        if (get(r.getUuid()) != null) { // такое резюме есть в storage
+            for (int i = 0; i < size; i++) {
+                if (storage[i].getUuid() == r.getUuid()) { // ищем индекс резюме в массиве
+                    storage[i] = r;
+                }
+            }
+        } else {
+            System.out.println("Резюме " + r.getUuid() + " не найдено.");
+        }
     }
 
     public void save(Resume r) { // три проверки - что на входе не null, что резюме не найдено и что storage переполнено
@@ -26,9 +38,9 @@ public class ArrayStorage {
                 System.out.println("Переполнение массива резюме");
             } else {
                 if (get(r.getUuid()) != null) { //если такое резюме уже есть
-                    System.out.println("Резюме + " + r.getUuid() + " уже есть.");
+                    System.out.println("Резюме " + r.getUuid() + " уже есть.");
                 } else {
-                    storage[size] = r; //сохраняю в последнюю свободную ячейку
+                    storage[size] = r; // сохраняю в последнюю свободную ячейку
                     size++;
                 }
             }
@@ -41,7 +53,6 @@ public class ArrayStorage {
                 return storage[i];
             }
         }
-        System.out.println("Резюме + " + uuid + " не найдено.");
         return null; // если не совпал с uuid
     }
 
@@ -55,7 +66,7 @@ public class ArrayStorage {
                 }
             }
         } else {
-            System.out.println("Резюме + " + uuid + " не найдено.");
+            System.out.println("Резюме " + uuid + " не найдено.");
         }
     }
 
@@ -63,8 +74,8 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        Resume[] allResume = new Resume[size]; //создаем новый массив длиной с число резюме
-        System.arraycopy(storage, 0, allResume, 0, size); //копируем в него часть массива с резюме
+        Resume[] allResume = new Resume[size]; // создаем новый массив длиной с число резюме
+        System.arraycopy(storage, 0, allResume, 0, size); // копируем в него часть массива с резюме
         return allResume;
     }
 
