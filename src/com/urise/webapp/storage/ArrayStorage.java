76823f124b1acfa676cbe280a1b3bde0e51242d6
Vasili Bandarenka <darken123@inbox.ru>
2022-11-02 12:34:28 +0300

    создал пакеты
    создал пакеты, разнес классы по пакетам, исправил доступы - прописал private

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,3 +1,7 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,21 +9,21 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     int size; //число резюме
 
-    void clear() {
+    public void clear() {
         for (int i = 0; i < size; i++) { //стираю резюме
             storage[i] = null;
         }
         size = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
         if (r.uuid != null) { //если на входе не null
             storage[size] = r; //сохраняю в последнюю свободную ячейку
             size++;
         }
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) { //если элемент совпал с uuid
                 return storage[i];
@@ -28,7 +32,7 @@ public class ArrayStorage {
         return null; //если не совпал с uuid
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) { //ищем резюме uuid
                 System.arraycopy(storage, i + 1, storage, i, size - i); //сдвигаю массив начиная с элемента i
@@ -40,13 +44,13 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         Resume[] allResume = new Resume[size]; //создаем новый массив длиной с число резюме
         System.arraycopy(storage, 0, allResume, 0, size); //копируем в него часть массива с резюме
         return allResume;
     }
 
-    int size() {
+    public int size() {
         return size;
     }
 }
