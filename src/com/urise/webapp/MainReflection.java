500089d4f9b41ef9492c6da32881c358b47052b8
Vasili Bandarenka <darken123@inbox.ru>
2022-11-18 22:18:49 +0300

    добавил класс MainReflection в нем вызов to string через invoke

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -9,30 +9,25 @@ import java.lang.reflect.Method;
 public class MainReflection {
     public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume("test UUID");
-        //Resume r = new Resume(); конструктор без параметров генерирует рандом uuid
-        System.out.println("\nr= " + r);
+        System.out.println("\nr - " + r);
 
         Field field = r.getClass().getDeclaredFields()[0];
-        System.out.println("can access before " + field.canAccess(r));
-        System.out.println("can access try " + field.trySetAccessible());
-        //field.setAccessible(true); //вариант вместо trySetAccessible
-        System.out.println("can access after try " + field.canAccess(r));
-        System.out.println("field.getName(r) " + field.getName());
-        System.out.println("field.get(r) " + field.get(r));
+        System.out.println("Can access before - " + field.canAccess(r));
+        System.out.println("SetAccessible return - " + field.trySetAccessible());
+        System.out.println("Can access after try - " + field.canAccess(r));
+        System.out.println("\nfield - " + field);
+        System.out.println("field.getName - " + field.getName());
+        System.out.println("field.get(r) - " + field.get(r));
         field.set(r, "New uuid");
-        System.out.println(r);
+        System.out.println("After change - " + r);
 
-        Method[] methods = r.getClass().getMethods(); // вывожу все имена методов
+        Method[] methods = r.getClass().getMethods();
         for (Method m : methods) {
-            //System.out.println(m); //полное имя
             System.out.println(m.getName());
         }
-        //HW4 В MainReflection вызовите у Resume, через отражение, метод toString. Выведите результат на консоль
-        System.out.println("\nЧерез invoke\n");
+        System.out.println("\nString Через invoke\n");
         Method method = r.getClass().getMethod("toString");
         System.out.println(method);
         System.out.println(method.invoke(r));
-        //r.toString();
-
     }
 }
